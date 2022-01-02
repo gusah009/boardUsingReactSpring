d5b0c491e86c9abaf04cde92f7cf27b0ffaf6814
핸모정 <gusah009@naver.com>
2022-01-03 00:29:34 +0900

    fix : 프론트 페이지 submit 안되던 문제, author가 없던 문제 해결

--- frontend/src/Board/Write.js
+++ frontend/src/Board/Write.js
@@ -6,15 +6,24 @@ import "./Board.css";
 function Write() {
   const [Title, SetTitle] = useState("");
   const [Content, SetContent] = useState("");
+  const [Author, SetAuthor] = useState("");
 
   const titleHandler = (e) => {
     e.preventDefault();
+    console.log(Title);
     SetTitle(e.target.value);
   };
 
   const contentHandler = (e) => {
     e.preventDefault();
+    console.log(Content);
     SetContent(e.target.value);
+  };
+
+  const authorHandler = (e) => {
+    e.preventDefault();
+    console.log(Author);
+    SetAuthor(e.target.value);
   };
 
   const submitHandler = (e) => {
@@ -22,10 +31,12 @@ function Write() {
     // state에 저장한 값을 가져옵니다.
     console.log(Title);
     console.log(Content);
+    console.log(Author);
 
     let body = {
       title: Title,
       content: Content,
+      author: Author
     };
 
     axios.post("/board/write", body).then((res) => console.log(res));
@@ -34,23 +45,28 @@ function Write() {
   return (
     <>
       <div className="form-wrapper">
-        <form onSubmit={submitHandler}>
-          <input
-            className="title-input"
-            type="text"
-            placeholder="제목"
-            value={Title}
-            onChange={titleHandler}
-          />
-          <textarea
-            className="text-area"
-            placeholder="내용"
-            value={Content}
-            onChange={contentHandler}
-          ></textarea>
-        </form>
+        <input
+          className="title-input"
+          type="text"
+          placeholder="제목"
+          value={Title}
+          onChange={titleHandler}
+        />
+        <input
+          className="title-input"
+          type="text"
+          placeholder="작성자"
+          value={Author}
+          onChange={authorHandler}
+        />
+        <textarea
+          className="text-area"
+          placeholder="내용"
+          value={Content}
+          onChange={contentHandler}
+        ></textarea>
+        <button className="submit-button" type="submit" onClick={submitHandler}>입력</button>
       </div>
-      <button className="submit-button" type="submit">입력</button>
     </>
   );
 }
